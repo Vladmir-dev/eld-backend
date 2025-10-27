e19a04a982c86cb6a2b057f2331fd66371bc2628
Vladmir-dev <tempestpius70@gmail.com>
2025-10-27 22:11:19 +0300

    updated database url

--- eld_backend/settings.py
+++ eld_backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,12 +96,33 @@ WSGI_APPLICATION = 'eld_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            conn_health_check=True,
+        )
     }
-}
+    # Security note: Use HTTPS connection if possible (e.g., 'ssl_require': True)
+    # For many Vercel deployments, this is necessary:
+    DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
+
+else:
+    # Use SQLite for local development if no DATABASE_URL is set
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
